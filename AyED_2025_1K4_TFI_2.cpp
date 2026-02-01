87a2cef53fd92f10e02d7ae30b9d1fc2a3eeab3c
BurgosManuel <trapecioinv@gmail.com>
2026-01-31 22:16:15 -0300

    Primer commit

---
+++ AyED_2025_1K4_TFI_2.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h> // Libreria estandar para usar printf() y scanf().
+#include <stdlib.h> // Libreria para hacer uso de la funcion rand().
+#include <time.h> // Libreria utilizada para medir el tiempo de ejecucion de los algoritmos de ordenamiento.
+
+main()
+{
+
+}
